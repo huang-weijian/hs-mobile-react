e5d343eebcf076fd8fdf53f460120a6f896dfefb
huangweijian <1411932776@qq.com>
2021-06-04 19:34:36 +0800

    初始化Button组件

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export declare interface ButtonProps {
+}
+
+function Button(props: ButtonProps) {
+    return (
+        <div></div>
+    );
+}
+
+export default Button;
