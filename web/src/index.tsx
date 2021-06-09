3ef0ce14b202bc7e069f62dd627f69b5bbe874d7
huangweijian <1411932776@qq.com>
2021-06-09 15:30:13 +0800

    删除多余文件、测试web-build

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,6 @@
 import "react-app-polyfill/ie9";
 import "react-app-polyfill/stable";
 
-import "./index.css";
 import { createElement } from "react";
 import * as ReactDom from "react-dom";
 import App from "./App";
