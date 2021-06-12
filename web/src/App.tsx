2936804e2d0331146f737f156e3de11abd01fd9d
huangweijian <1411932776@qq.com>
2021-06-12 16:16:50 +0800

    初始化react-router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,38 @@
-import { createElement } from "react";
+import { ComponentType, createElement, lazy, Suspense } from "react";
 import "./css/app.less";
-import DemoButton from "./views/DemoButton";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import { RouteComponentProps } from "react-router";
+
+const Demobutton = lazy(() => import("./views/DemoButton"));
+const Home = lazy(() => import("./views/Home/Home"));
+
+function ViewLoading() {
+  return <div>View Loading......</div>;
+}
+
+function warpRouteComponent(
+  Component: ComponentType<RouteComponentProps>
+): ComponentType<RouteComponentProps> {
+  return (props: RouteComponentProps) => (
+    <Suspense fallback={<ViewLoading></ViewLoading>}>
+      <Component {...props}></Component>
+    </Suspense>
+  );
+}
 
 function App() {
   return (
     <div className={"app"}>
-      <DemoButton></DemoButton>
+      <HashRouter basename={"/"}>
+        <Switch>
+          <Route path={"/home"} component={warpRouteComponent(Home)}></Route>
+          <Route
+            path={"/button"}
+            component={warpRouteComponent(Demobutton)}
+          ></Route>
+          <Route path={"/*"} render={() => <div>It is nothing</div>}></Route>
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
---
+++ web/src/views/Home/Home.tsx
@@ -0,0 +1,15 @@
+import { createElement } from "react";
+
+export declare interface HomeProps {}
+
+function Home(props: HomeProps) {
+  return (
+    <div>
+      <div>
+        <a href="#/button">button</a>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
