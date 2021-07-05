56474850ccaff3a0db4ba7de3ecff4c6531d03f5
huang-weijian <1411932776@qq.com>
2021-07-05 18:50:06 +0800

    complete Picker and PickerScrollItem Component and test page

--- web/src/views/DemoPickerScrollItem/DemoPickerScrollItem.tsx
+++ web/src/views/DemoPickerScrollItem/DemoPickerScrollItem.tsx
@@ -1,14 +1,27 @@
 import SplitLine from "@/components/SplitLine/SplitLine";
 import { PickerScrollItem } from "@hs";
+import { useState } from "react";
 
 export declare interface IDemoPickerScrollItemProps {}
 
 function DemoPickerScrollItem(props: IDemoPickerScrollItemProps) {
+  let [list] = useState(
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => ({
+      id: String(item),
+      text: String(item),
+      value: String(item),
+    }))
+  );
+  let [current, setCurrent] = useState<any>();
   return (
     <div>
       <SplitLine title={"PickerScorllItem"}></SplitLine>
-      <div style={{ height: "200px" }}>
-        <PickerScrollItem data={[]}></PickerScrollItem>
+      <p>current：{JSON.stringify(current)}</p>
+      <div style={{ height: "250px" }}>
+        <PickerScrollItem
+          data={list}
+          onSelect={(dataItem) => setCurrent(dataItem)}
+        ></PickerScrollItem>
       </div>
     </div>
   );
