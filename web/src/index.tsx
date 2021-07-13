272cbdf64c9d5e39e7da90a4cb7be52bc6a950e6
huang-weijian <1411932776@qq.com>
2021-07-13 14:40:17 +0800

    remove fastclick

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,9 +2,6 @@ import "react-app-polyfill/ie9";
 import "react-app-polyfill/stable";
 import { createElement } from "react";
 import { render } from "react-dom";
-import fastclick from "fastclick";
 import App from "./App";
 
-// @ts-ignore
-fastclick.attach(document.body);
 render(<App></App>, document.getElementById("app"));
